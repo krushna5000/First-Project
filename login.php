7ffaa95ab6915dfc5effd7be184d192a747ec89c
krushna5000 <158137389+krushna5000@users.noreply.github.com>
2024-05-14 12:33:39 +0530

    comment7

--- login.php
+++ login.php
@@ -2,25 +2,25 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "login";
+$dbname = "aartidb";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn ) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 // Process registration form data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
-    $password = $_POST['password'];
-    // $contact = $_POST['contact'];
-    // $amount = $_POST['amount'];
+    $email = $_POST['email'];
+    $contact = $_POST['paswword'];
+   
 
     // Insert user data into the database
-    $sql = "INSERT INTO pay (Name, Email,password) VALUES ('$name', '$password')";
+    $sql = "INSERT INTO `info` (`name`, `email`, `password`) VALUES ('$name', '$email', '$password');)";
 
     if ($conn->query($sql) === TRUE) {
       echo "<script> alert('Data Inserted successful') </script>";
@@ -29,23 +29,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// $sql = "SELECT username, password FROM users";
-// $result = $conn->query($sql);
+$sql = "SELECT username, password FROM users";
+$result = $conn->query($sql);
 
-// $data=[];
+$data=[];
 
-// if ($result->num_rows > 0) {
-//     // Output data of each row
-//     while($row = $result->fetch_assoc()) {
-//         echo " - username: " . $row["username"]. " - password: " . $row["password"]. "<br>";
-//         $data[]=$row;
-//     }
-// } else {
-//     echo "0 results";
-// }
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo " - username: " . $row["username"]. " - password: " . $row["password"]. "<br>";
+        $data[]=$row;
+    }
+} else {
+    echo "0 results";
+}
 
 // Close connection
 $conn->close();
-
-
 ?>
